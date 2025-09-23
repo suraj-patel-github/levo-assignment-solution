be295c871c495ce2a18507b18f807ae738efded6
suraj5.patel <suraj5.patel@ril.com>
2025-09-23 12:51:14 +0000

    removed aal the errors. Go mod tidy.

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-    "database/sql"
-    "log"
-    "net/http"
-    "os"
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
 
-    _ "github.com/lib/pq"
+	"levo-schema-service/internal/storage"
+	"levo-schema-service/pkg/repository"
+	"levo-schema-service/pkg/service"
+	"levo-schema-service/pkg/transport"
 
-    "schema-versioner/internal/storage"
-    "schema-versioner/pkg/repository"
-    "schema-versioner/pkg/service"
-    "schema-versioner/pkg/transport"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-    dsn := os.Getenv("DB_DSN") // e.g. postgres://user:pass@localhost:5432/schema_db?sslmode=disable
-    db, err := sql.Open("postgres", dsn)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+	dsn := os.Getenv("DB_DSN") // e.g. postgres://user:pass@localhost:5432/schema_db?sslmode=disable
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-    repo := repository.New(db)
-    fs := storage.NewFileStore("./uploads")
-    svc := service.New(repo, fs)
+	repo := repository.New(db)
+	fs := storage.NewFileStore("./uploads")
+	svc := service.New(repo, fs)
 
-    mux := http.NewServeMux()
-    mux.Handle("/upload", transport.UploadHandler(svc))
-    mux.Handle("/latest", transport.LatestHandler(svc))
-    mux.Handle("/version", transport.VersionHandler(svc))
+	mux := http.NewServeMux()
+	mux.Handle("/upload", transport.UploadHandler(svc))
+	mux.Handle("/latest", transport.LatestHandler(svc))
+	mux.Handle("/version", transport.VersionHandler(svc))
 
-    log.Println("Schema Versioner API running on :8080")
-    log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Println("Schema Versioner API running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
